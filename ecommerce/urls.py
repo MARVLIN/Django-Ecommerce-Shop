033640badc8e029e6dc4a4a318dadf7774b89073
MARVLIN <47501302+MARVLIN@users.noreply.github.com>
2021-11-03 11:43:35 +0000

    aa

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -31,4 +31,6 @@ urlpatterns = [
     path('', include('store.urls'), name='store'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
